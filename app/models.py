9e4974c7a56965ac09dda40c7ab8032efc021c7e
Connor Ladly-Fredeen <connorladlyfredeen@gmail.com>
2017-10-22 09:40:09 -0400

    Added more models

--- app/models.py
+++ app/models.py
@@ -8,8 +8,11 @@ from django.db import models
 @python_2_unicode_compatible
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    season = models.CharField(max_length=50)
     product_type = models.CharField(max_length=50)
+    winter = models.BooleanField(default=False)
+    spring = models.BooleanField(default=False)
+    summer = models.BooleanField(default=False)
+    fall = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
@@ -22,3 +25,23 @@ class Vendor(models.Model):
 
     def __str__(self):
         return self.name
+
+
+@python_2_unicode_compatible
+class Market(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    longitude = models.BigIntegerField()
+    latitude = models.BigIntegerField()
+
+    def __str__(self):
+        return self.name
+
+@python_2_unicode_compatible
+class Query(models.Model):
+    longitude = models.BigIntegerField()
+    latitude = models.BigIntegerField()
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.address
